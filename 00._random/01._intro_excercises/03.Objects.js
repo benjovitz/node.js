bbf8b4484d3e46d738428947abb6d9fcd5ca448d
benjovitz <dbenjovitz@gmail.com>
2023-08-22 16:03:28 +0200

    solved 03 and 04

--- 00._random/01._intro_excercises/03.Objects.js
+++ 00._random/01._intro_excercises/03.Objects.js
@@ -6,28 +6,33 @@
 const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
-
+console.log(myObj.message)
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+const person = {}
 
+person.name="Daniel Benjovitz"
+person.age=23
+person.height=183
 
 // --------------------------------------
 // Exercise 3 - Add a property 
 
 const stackOverflow = {};
-
 // make a rule called isAllowed and let the value be true
-
+stackOverflow.isAllowed=true
+console.log(stackOverflow.isAllowed)
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
 const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
-
-
+delete thisSong.description
+thisSong.about="Just a tribute."
+console.log(thisSong.about)
 // --------------------------------------
 
 
