489dc1204c2677e63cdd84f5303b18cea0163d1b
Daniel <dbenjovitz@gmail.com>
2023-09-07 08:36:46 +0200

    added debug.js

---
+++ 00._Miscellaneous/debug.js
@@ -0,0 +1,43 @@
+'use strict';
+
+myConstant = "Hello World";
+
+eval("let x = 2");
+x = 3;
+
+var public = "public variable";
+
+const defaultValue;
+
+const message = "";
+
+let console = "Output";
+
+delete console;
+
+delete arguments;
+
+const arguments = [1, 2, 3];
+
+function alerting() {
+    message = console;
+ }
+
+ delete 
+ 
+
+function searchingForTrouble() {
+    trouble = true;
+}
+
+function logMessage(guard="") {
+    message = "Everything is fine.";
+    if (!guard) {
+        // The task is to print "Everything is fine." here.
+        console.log(message);
+    }
+}
+
+logMessage();
+
+everythingIsFine(true);
