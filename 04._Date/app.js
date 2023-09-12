c302d936a898b2874949608909d4f40ca8c8bc4d
Daniel <dbenjovitz@gmail.com>
2023-09-12 10:33:01 +0200

    completed date tasks

--- 04._Date/app.js
+++ 04._Date/app.js
@@ -5,9 +5,34 @@ const app = express()
 
 console.log(Date())
 
-console.log(new Date())
+console.log(new Date().getMonth())
 
 console.log(Date.now())
+
+app.get("/date", (req, res) => {
+    res.send({data:{today: Date()}})
+})
+
+app.get("/month", (req, res) => {
+    const date = Date().split(" ")
+    res.send({data:{month: date[1]}})
+})
+
+app.get("/month2", (req, res) => {
+    const currentMonth = new Date().toLocaleDateString("en-US", {month: "long"})
+    res.send({data: currentMonth})
+})  
+
+app.get("/day", (req, res) => {
+    const date = Date().split(" ")
+    res.send({ data:{ day: date[0] } })
+})
+
+app.get("/day2", (req, res) => {
+    const currentDay = new Date().toLocaleDateString("en-US", {weekday: "long"})
+    res.send({data: currentDay})
+})
+
 
 const PORT = 8080
 app.listen(PORT, (req, res) => {
