82d83556d51449a7cc689bdc28ad94856751dfa2
benjovitz <dbenjovitz@gmail.com>
2023-09-12 10:01:53 +0200

    added date task

--- 04._Date/app.js
+++ 04._Date/app.js
@@ -2,4 +2,14 @@ const express = require("express")
 
 const app = express()
 
-app.listen(8080)
+
+console.log(Date())
+
+console.log(new Date())
+
+console.log(Date.now())
+
+const PORT = 8080
+app.listen(PORT, (req, res) => {
+    console.log("Server running on port:", PORT)
+})
