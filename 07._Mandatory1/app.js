c6375c830e9f6b88f11ee12961936cd0588c706b
benjovitz <dbenjovitz@gmail.com>
2023-10-01 22:46:41 +0200

    changed to ssr

--- 07._Mandatory1/app.js
+++ 07._Mandatory1/app.js
@@ -5,10 +5,37 @@ export const app = express()
 app.use(express.json())
 app.use(express.static("public"))
 
+/*---------------------------------------HTML-----------------------------------------------*/ 
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./public/homepage.html"))
 })
 
+app.get("/javascript", (req, res) => {
+    res.sendFile(path.resolve("./public/javascript/topics.html"))
+})
 
+app.get("/javascript/basics", (req, res) => {
+    res.sendFile(path.resolve("./public/javascript/topics/jsbasics.html"))
+})
+
+app.get("/javascript/declarations", (req, res) => {
+    res.sendFile(path.resolve("./public/javascript/topics/declarations.html"))
+})
+
+app.get("/javascript/objects", (req, res) => {
+    res.sendFile(path.resolve("./public/javascript/topics/objects.html"))
+})
+
+app.get("/javascript/arrays", (req, res) => {
+    res.sendFile(path.resolve("./public/javascript/topics/arrays.html"))
+})
+
+app.get("/javascript/arrays.js", (req, res) => {
+    res.sendFile(path.resolve("./public/javascript/topics/arrays.js"))
+})
+
+
+/*---------------------------------------ROUTES-----------------------------------------------*/
 
 app.listen(8080)
